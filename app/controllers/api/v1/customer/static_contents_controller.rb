2a8a7a91646ede70021712aeecddf8e6eb8404ca
Sonu Kumar <sonu.kumar1@mobiloitte.in>
2018-09-01 13:16:07 +0530

    Terms & condition api

--- app/controllers/api/v1/customer/static_contents_controller.rb
+++ app/controllers/api/v1/customer/static_contents_controller.rb
@@ -10,4 +10,18 @@ class Api::V1::Customer::StaticContentsController < ApplicationController
     render json: {responseCode: 200, responseMessage: "Content fetched successfully.",
                   content: {title: @static_content.try(:title), description: strip_tags(@static_content.try(:description)).try(:chomp)}}    
   end
+
+   def privacy_policy
+    @privacy_policy = StaticPage.where(title: "Privacy Policy").first
+    return render json: {responseCode: 200, responseMessage: t('privacy_policy.successfully'),
+              about_us: {title: @privacy_policy.try(:title), description: strip_tags(@privacy_policy.try(:description)).chomp}
+             }    
+  end
+ 
+  def terms_and_condition
+    @terms_and_services = StaticPage.where(title: "Terms & Conditions").first
+    return render json: {responseCode: 200, responseMessage: t('terms_and_services.successfully'),
+              about_us: {title: @terms_and_services.try(:title), description: strip_tags(@terms_and_services.try(:description)).chomp}
+             }    
+  end
 end
